cfc180d2619fb2a40e088db216cec9668b9cc61d
perlaharibabu <perla.haribabu@mahindra.com>
2026-05-07 11:59:32 +0530

    bg image changes

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-[url('/images/bg-image.jpg')] bg-cover bg-center h-screen ">
+    <div className="bg-[url('/images/bg-image-one.jpg')] bg-cover bg-center h-screen">
       <div className="bg-yellow-500/40 h-full w-full flex items-center justify-center">
            <div className="bg-white rounded-lg shadow-lg text-center w-100">
             <h1 className="text-2xl font-bold text-gray-800
